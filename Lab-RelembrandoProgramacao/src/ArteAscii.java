80991432cdd136fce2774f015f2cae1d82dd7df1
Joao Danilo <joaodanilo1992@gmail.com>
2024-08-22 23:20:51 -0400

    Initialized Lab-RelembrandoProgramacao

---
+++ Lab-RelembrandoProgramacao/src/ArteAscii.java
@@ -0,0 +1,25 @@
+public class ArteAscii{
+	public static void main(String[] args){
+		
+		if(args.length == 0) return;
+
+		int quantidade = Integer.valueOf(args[0]);
+		
+		if (quantidade < 1) return;
+
+		for(int i = quantidade; i > 0; i--){
+			for(int j = 0; j < i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for(int i = 0; i < quantidade; i++){
+			for(int j = 0; j < i; j++){
+				System.out.print("*");
+			}
+			System.out.println("*");
+		}
+	}
+
+}
